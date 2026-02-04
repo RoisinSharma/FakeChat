8f2004ac34a24d843e6a958b867a5a7ad1389257
Ana Neves <asf25@sussex.ac.uk>
2026-02-04 17:12:45 +0000

    Update demographics.js
    updated debrief

--- experiment/demographics.js
+++ experiment/demographics.js
@@ -291,20 +291,114 @@ var demographics_debriefing = {
                         html:
                             "<img src='https://blogs.brighton.ac.uk/sussexwrites/files/2019/06/University-of-Sussex-logo-transparent.png' width='150px' align='right'/><br><br><br><br><br>" +
                             "<h2>Debriefing</h2>" +
-                            "<p align='left'> The purpose of this study was to examine how the framing of an interaction partner as an AI versus human influences people's trust, empathy, and perceived social presence during online personal conversations. In this experiment, some conversations were framed as being human-AI interactions, and others as being between two humans. However, all conversations were fictional and created specifically for this study." +
+                            "<p align='left'> The purpose of this study was to examine how the framing of an interaction partner as an AI versus human influences people's trust, empathy, and perceived social presence during online personal conversations." +
+                            " In this experiment, some conversations were framed as being human-AI interactions, and others as being between two humans. " +
+                            "However, all conversations were <b>fictional</b> and <b>created</b> specifically for this study <b>using ChatGPT-5.2.</b>" +
+                            "<p>We apolagise for the the necessary deception used in this study. Decepetion was essential to maintain validity of our experimenal manipulation." +
+                            "We hope you understand the rationale behind this decision." +
                             "<p>We were also interested in how individual differences - such as loneliness, mental wellbeing, interoceptive awareness, AI familiarity and social motivation - relate to these reactions. Understanding these factors gives insight into why and how people may respond differently to online social partners, informing future research surrounding AI use.</p>" +
                             "<p align='left'><b>Thank you again!</b> Your participation in this study will be kept completely confidential. If you have any questions or concerns about the project, please contact Dr Dominique Makowski (<i style='color:DodgerBlue;'>D.Makowski@sussex.ac.uk</i>), Riehana Aziz (<i style='color:DodgerBlue;'>ra549@sussex.ac.uk</i>), and/or Emma Benn (<i style='color:DodgerBlue;'>eb672@sussex.ac.uk</i>).</p>" +
-                            "<h4> To complete your participation in this study, click on 'Continue' and <b>wait until your responses have been successfully saved</b> before closing the tab.</h4><br>" +
-                            "<p style='color: red; font-weight: bold;'>We appreciate that some of the content in this study can be heavy and distressing. If you or someone you know is struggling with their mental health, please do not hesitate to reach out to the following charities for further support.</p>" +
-                            "<p style='color: red;'>If you or someone you know is in immediate danger or thinking about ending their life, please call 999 or go to your nearest A&E.</p>" +
-                            "<p style='color: red;'><b> 24/7 Crisis lines:</b> </p>" +
-                            "<p style='color: red;'>Call The Samaritans on 116 123</p>" +
-                            "<p style='color: red;'>Text 'SHOUT' to 85258</p>" +
-                            "<p style='color: red;'><b>Support via charities:</b></p>" +
-                            "<p style='color: red;'>Call the Mind welfare line, open Monday - Friday (9 am to 5 pm): 0300 123 3393 or visit their website for tools and strategies</p>" +
-                            "<a style='color: red;' href='https://www.mind.org.uk/need-urgent-help/using-this-tool/'>www.mind.org.uk/need-urgent-help</a>" +
-                            "<p style='color: red;'>Mental Health Foundation: </p><a style='color: red;' href='https://www.mentalhealth.org.uk/'>www.mentalhealth.org.uk</a>" +
-                            "<p style='color: red;'><b>Local support:</b> Use the government website to find support services near you </p><a style='color: red;' href='https://www.nhs.uk/nhs-services/mental-health-services/'>www.nhs.uk/nhs-services/mental-health-services</a><br><br>",
+                            "<p align='center' style='color: black; font-size: 30px;'><b>To complete your participation in this study, click on 'Continue' and wait until your responses have been successfully saved before closing the tab.</b></p>" +
+                            //Resources
+
+                            // "<p style='color: red; font-weight: bold;'>We appreciate that some of the content in this study can be heavy and distressing. If you or someone you know is struggling with their mental health, please do not hesitate to reach out to the following charities for further support.</p>" +
+                            // "<p>If you or someone you know is in immediate danger or thinking about ending their life, please call 999 or go to your nearest A&E.</p>" +
+                            // "<p style='color: red;'><b> 24/7 Crisis lines:</b> </p>" +
+                            // "<p>Call The <b>Samaritans</b> on <b>116 123</b></p>" +
+                            // "<>Text 'SHOUT' to 85258</p>" +
+                            // "<p style='color: red;'><b>Support via charities:</b></p>" +
+                            // "<p style='color: red;'>Call the Mind welfare line, open Monday - Friday (9 am to 5 pm): 0300 123 3393 or visit their website for tools and strategies</p>" +
+                            // "<a style='color: red;' href='https://www.mind.org.uk/need-urgent-help/using-this-tool/'>www.mind.org.uk/need-urgent-help</a>" +
+                            // "<p style='color: red;'>Mental Health Foundation: </p><a style='color: red;' href='https://www.mentalhealth.org.uk/'>www.mentalhealth.org.uk</a>" +
+                            // "<p style='color: red;'><b>Local support:</b> Use the government website to find support services near you </p><a style='color: red;' href='https://www.nhs.uk/nhs-services/mental-health-services/'>www.nhs.uk/nhs-services/mental-health-services</a><br><br>",
+
+                            "<p align='center' style='color: red; font-size: 32px;'><b>Support Resources</b></p>" +
+                            "<p>We appreciate that some of the content in this study may be personal or distressing. " +
+                            "Below are resources you can access for support related to mental health, psychiatric conditions, and wellbeing. " +
+                            "Your participation is appreciated, and we encourage you to reach out if you need help.</p>" +
+                            //Imediate crisis
+                            "<p align='left' style='color: red; font-size: 16px;'><b>Immediate Crisis Support:</b></p>" +
+                            " If you or someone you know is in <b>immediate danger or thinking about ending their life</b>, please call <b>999</b> or go to your nearest A&E.</li>" +
+                            "<p><b>Mental Health Support:</b></p>" +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li>Call The Samaritans (24/7) on 116 123</li>" +
+                            "<li>Text 'SHOUT' to 85258 for 24/7 text support</li>" +
+                            "<li>Mind Welfare Line (Monday–Friday, 9am–5pm): 0300 123 3393 | <a href='https://www.mind.org.uk/need-urgent-help' target='_blank'>Visit website</a></li>" +
+                            "<li>Mental Health Foundation: <a href='https://www.mentalhealth.org.uk' target='_blank'>www.mentalhealth.org.uk</a></li>" +
+                            "<li>Local NHS mental health services: <a href='https://www.nhs.uk/nhs-services/mental-health-services' target='_blank'>Find local support</a></li>" +
+                            "</ul>" +
+                            // Neurodevelopment Conditions
+                            "<p><b>Psychiatric and Neurodevelopmental Conditions:</b></p>" +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li><b>Addiction:</b> FRANK Drugs Helpline: 0300 123 6600 | <a href='https://www.talktofrank.com/' target='_blank'>Visit website</a></li>" +
+                            "<li><b>ADHD:</b>" +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li>ADHD UK: <a href='https://adhduk.co.uk/' target='_blank'>www.adhduk.co.uk</a> | Support groups: <a href='https://adhduk.co.uk/support/' target='_blank'>https://adhduk.co.uk/support/</a></li>" +
+                            "<li>ADHD Adult UK: <a href='https://www.adhdadult.uk/' target='_blank'>www.adhdadult.uk</a> | Parenting guide (PDF): <a href='https://www.adhdadult.uk/wp-content/uploads/2023/06/Parenting-children-with-ADHD-guide.pdf' target='_blank'>Download PDF</a></li>" +
+                            "</ul>" +
+                            "<li><b>Autism:</b> " +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li>National Autistic Society: <a href='https://www.autism.org.uk/' target='_blank'>www.autism.org.uk</a></li>" +
+                            "<li>Ambitious about Autism: Call 020 8815 5444 | <a href='https://www.ambitiousaboutautism.org.uk' target='_blank'>Visit website</a></li>" +
+                            "<li>Autism Central: <a href='https://www.autismcentral.org.uk/' target='_blank'>www.autismcentral.org.uk</a></li>" +
+                            "</ul>" +
+                            "</ul>" +
+                            //Other Disorders
+                            "<p><b>Disorder-specific Support:</b></p>" +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li>Bipolar Disorder: <a href='https://www.bipolaruk.org/' target='_blank'>Bipolar UK</a> | Mind support: <a href='https://www.mind.org.uk/' target='_blank'>Mind</a></li>" +
+                            "<li>Borderline Personality Disorder (BPD): <a href='https://www.nhs.uk/mental-health/conditions/borderline-personality-disorder/' target='_blank'>NHS Info</a></li>" +
+                            "<li>Major Depressive Disorder (MDD): <a href='https://www.mind.org.uk/information-support/types-of-mental-health-problems/depression/' target='_blank'>Mind: Depression</a></li>" +
+                            "<li>Obsessive-Compulsive Disorder (OCD): <a href='https://www.ocduk.org/' target='_blank'>OCD-UK</a></li>" +
+                            "<li>Panic Disorder: <a href='https://www.mind.org.uk/information-support/types-of-mental-health-problems/anxiety/' target='_blank'>Mind: Anxiety & Panic</a></li>" +
+                            "<li>Post-Traumatic Stress Disorder (PTSD): <a href='https://www.mentalhealth.org.uk/conditions/ptsd' target='_blank'>Mental Health Foundation</a></li>" +
+                            "<li>Schizophrenia: <a href='https://www.nhs.uk/mental-health/conditions/schizophrenia/' target='_blank'>NHS Info</a></li>" +
+                            "<li>Social Anxiety Disorder (Social Phobia): <a href='https://www.mind.org.uk/information-support/types-of-mental-health-problems/social-anxiety/' target='_blank'>Mind: Social Anxiety</a></li>" +
+                            "<li>Specific Phobias: <a href='https://www.nhs.uk/mental-health/conditions/phobias/' target='_blank'>NHS Info</a></li>" +
+                            "<li>Eating Disorders (e.g., Anorexia, Bulimia, Binge Eating): <a href='https://www.nhs.uk/mental-health/conditions/eating-disorders/' target='_blank'>NHS Info</a> | <a href='https://www.beateatingdisorders.org.uk/' target='_blank'>Beat Eating Disorders</a></li>" +
+                            "</ul>" +
+                            //Physical ans somatic Health
+                            "<p align='left'><b>Support Resources for Physical and Somatic Health Conditions</b></p>" +
+                            "<p>If any of the following conditions raised concerns for you, or if you would like more information and support, the following organisations and resources may be helpful.</p>" +
+                            "<ul style='padding-left: 30px;'>" +
+                            "<li><b>Musculoskeletal and Pain Conditions:</b><br>" +
+                            "Hypermobility Syndrome (e.g., Ehlers-Danlos), Fibromyalgia, Chronic Fatigue Syndrome, Chronic Pain, Back Pain, Muscle Tension<br>" +
+                            "<a href='https://www.versusarthritis.org/' target='_blank'>Versus Arthritis</a> – Advice and support for musculoskeletal conditions<br>" +
+                            "<a href='https://www.fibromyalgia.org/' target='_blank'>Fibromyalgia Support UK</a> – UK-based resources and support groups<br>" +
+                            "<a href='https://www.actionforme.org.uk/' target='_blank'>Action for ME</a> – Support for Myalgic Encephalomyelitis / Chronic Fatigue Syndrome" +
+                            "</li>" +
+                            "<li><b>Dermatological and Skin Conditions:</b><br>" +
+                            "Skin rashes, Eczema, Psoriasis, Sjogren's Syndrome<br>" +
+                            "<a href='https://www.nhs.uk/conditions/skin-conditions/' target='_blank'>NHS: Skin Conditions</a><br>" +
+                            "<a href='https://www.psoriasis.org.uk/' target='_blank'>Psoriasis Association</a> – Information and support" +
+                            "</li>" +
+                            "<li><b>Cardiovascular Conditions:</b><br>" +
+                            "Chest Pain, Cardiac Arrhythmia, Hypertension, Hypotension<br>" +
+                            "<a href='https://www.bhf.org.uk/' target='_blank'>British Heart Foundation</a> – Advice, support, and local services" +
+                            "</li>" +
+                            "<li><b>Gastrointestinal Conditions:</b><br>" +
+                            "Irritable Bowel Syndrome (IBS), GERD, Crohn's Disease, Ulcerative Colitis, Celiac Disease, Gluten or Lactose Intolerance<br>" +
+                            "<a href='https://www.crohnsandcolitis.org.uk/' target='_blank'>Crohn's & Colitis UK</a> – Support and information<br>" +
+                            "<a href='https://www.coeliac.org.uk/' target='_blank'>Coeliac UK</a> – Advice and resources" +
+                            "</li>" +
+                            "<li><b>Respiratory Conditions:</b><br>" +
+                            "Shortness of Breath, Asthma, COPD, Sleep Apnea, Chronic Bronchitis<br>" +
+                            "<a href='https://www.asthma.org.uk/' target='_blank'>Asthma UK</a> – Advice and support<br>" +
+                            "<a href='https://www.blf.org.uk/' target='_blank'>British Lung Foundation</a> – Resources for COPD and lung health" +
+                            "</li>" +
+                            "<li><b>Neurological Conditions:</b><br>" +
+                            "Nausea/Vomiting, Dizziness, Migraine, Neuropathy, Epilepsy, Multiple Sclerosis (MS)<br>" +
+                            "<a href='https://www.epilepsy.org.uk/' target='_blank'>Epilepsy Society</a> – Information, advice, and support groups<br>" +
+                            "<a href='https://www.mssociety.org.uk/' target='_blank'>MS Society UK</a> – Support for Multiple Sclerosis<br>" +
+                            "<a href='https://www.migrainetrust.org/' target='_blank'>Migraine Trust</a> – Resources and patient support" +
+                            "</li>" +
+                            "<li><b>Genitourinary Conditions:</b><br>" +
+                            "Frequent Urination, Endometriosis, Interstitial Cystitis, Chronic Pelvic Pain Syndrome<br>" +
+                            "<a href='https://www.endometriosis-uk.org/' target='_blank'>Endometriosis UK</a> – Information and support<br>" +
+                            "<a href='https://www.ic-network.org/' target='_blank'>Interstitial Cystitis Support Network UK</a> – Guidance and resources" +
+                            "</li>" +
+                            "</ul>" +
+                            "<p>We encourage you to <b>reach out to any of the above services</b> if you feel <b>you need support or advice.</b> Your wellbeing is important, and there are resources available to help at any time.</p>",
                     },
                 ],
             },

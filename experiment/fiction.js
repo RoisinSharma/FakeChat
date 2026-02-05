f8c6c404b90abe9acb1694e8f518c6c8dc5e671a
Róisín Sharma <rs843@sussex.ac.uk>
2026-02-05 18:58:39 +0000

    Update fiction.js

--- experiment/fiction.js
+++ experiment/fiction.js
@@ -285,10 +285,8 @@ var phase1_vignette = {
             </div>
         `
     },
-    choices: ['Continue'],
-    button_html: '<button style="background-color:#FFFFFF; color:#474747; padding:12px 24px; border:2px solid #D9D9D9; border-radius:8px; font-size:18px; cursor:pointer; margin-top: 50px;">Continue</button>',
+    choices: ['<button style="background-color:#FFFFFF; color:#474747; padding:12px 24px; border:2px solid #D9D9D9; border-radius:8px; font-size:18px; cursor:pointer;">Continue</button>'],
     trial_duration: null,
-    
     data: function () {
         return {
             screen: "vignette_image1",
